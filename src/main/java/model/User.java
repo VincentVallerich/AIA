3dbd4ee3191539a4bcc16a2f6d3d64595845ee7e
SimonMoreau10 <simon.moreau4@gmail.com>
2022-01-03 09:43:03 +0100

    added the User class

---
+++ src/main/java/model/User.java
@@ -0,0 +1,46 @@
+package model;
+
+public class User {
+    private int id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public String getCompleteName() {
+        return this.getFirstName() + this.getLastName();
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
